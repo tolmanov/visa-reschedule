806da0ec0816e740f3e8c8aa318ea5c3ec2eea01
Peter Tolmanov <ptolmanov@nes.ru>
2022-06-12 12:10:37 +0100

    Setuptools imported in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
-from distutils.core import setup
+
+import setuptools
 
 
 def package_files(directory):
@@ -10,19 +11,18 @@ def package_files(directory):
     return paths
 
 
-
-setup(name='WebForms',
-      version='1.0',
-      description='Python Web Forms Automation',
-      author='Peter Tolmanov',
-      author_email='ptolmanov@nes.ru',
-      packages=['webforms'],
-      install_requires=[
-          'dynaconf',
-          'click',
-          'selenium',
-          'python-telegram-handler',
-      ],
-      data_files=[('config', package_files('config'))],
-      include_package_data=True,
-      )
+setuptools.setup(name='WebForms',
+                 version='1.0',
+                 description='Python Web Forms Automation',
+                 author='Peter Tolmanov',
+                 author_email='ptolmanov@nes.ru',
+                 packages=['webforms'],
+                 install_requires=[
+                     'dynaconf',
+                     'click',
+                     'selenium',
+                     'python-telegram-handler',
+                 ],
+                 data_files=[('config', package_files('config'))],
+                 include_package_data=True,
+                 )
